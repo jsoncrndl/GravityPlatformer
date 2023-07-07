e5ce0f54e4b0e131d35ab6984afd09566a72834c
Jason Crandall <jsonacrandall@gmail.com>
2023-07-07 12:49:17 -0600

    Sample code

---
+++ Assets/CollisionEvent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CollisionEvent : MonoBehaviour
+{
+    [SerializeField] float myVariable;
+    [SerializeField] bool secondVariable;
+    public Vector3 vectorVar;
+    private Color myColor;
+    public GameObject sceneRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Gravity.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    //enum GravityType { RADIAL, LINEAR }
+    public float gravity;
+}
