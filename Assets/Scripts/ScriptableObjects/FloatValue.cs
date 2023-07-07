4c570c640636ad30d6753adca775a1f72ddff339
Jason Crandall <jsonacrandall@gmail.com>
2023-07-07 15:27:50 -0600

    Added constants

---
+++ Assets/Scripts/ScriptableObjects/FloatValue.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Value/Float", fileName = "floatValue")]
+public class FloatValue : ScriptableObject
+{
+    [SerializeField] private float value;
+    public float Value => value;
+}
